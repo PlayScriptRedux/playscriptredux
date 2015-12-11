5dae5e3e620068add76d6e86e590d781c3ec974f
SushiHangover <sushihangover@outlook.com>
2015-12-10 16:06:21 -0800

    Fix broken monodevelop build

--- CI/monodevelop.osx.i386.sh
+++ CI/monodevelop.osx.i386.sh
@@ -4,11 +4,22 @@ git fetch mono
 git reset --hard
 git checkout upstream/master
 git pull mono master
+gut submodule sync
 git submodule update --init --recursive
-./configure --profile=mac --prefix=${HOME}/mono/monodevelop
+./configure --prefix=${HOME}/mono/monodevelop
+make clean
+
+# Quick fix for broken build
+pushd monodevelop//main/external/fsharpbinding/
+git reset --hard
+./configure.sh
+make
+popd
+
 make
 pushd main/build/MacOSX 
-make
+make render.exe
+make app
 ./make-dmg-bundle.sh
 popd
 popd
